3faf0901113157be64f1197d97aff57b3b463120
stevebosworth <steve.bosworth@shopify.com>
2015-06-17 15:19:39 -0400

    Add i18n tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler'
+require 'nokogiri'
+require 'htmlentities'
+require 'pry'
+require 'json'
+
+require_relative 'helpers/i18n_helper'
+require_relative 'helpers/html_helper'
+
+Bundler.require(:test)
