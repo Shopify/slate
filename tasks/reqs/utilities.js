5cfcc59a07c6b559bd55355124d516cd4f2a1210
Caroline Hill <mllegeorgesand@gmail.com>
2016-04-16 12:32:26 -0400

    Aspect ratio of icons taken into consideration

--- tasks/reqs/utilities.js
+++ tasks/reqs/utilities.js
@@ -59,22 +59,27 @@ var utilities = {
    */
   processSvg: function($, file) {
     var $svg = $('svg');
-    var $newSvg = $('<svg aria-hidden="true" focusable="false" role="presentation" />');
+    var $newSvg = $('<svg aria-hidden="true" focusable="false" role="presentation" class="icon" />');
     var fileName = file.relative.replace('.svg', '');
     var viewBoxAttr = $svg.attr('viewbox');
 
     // Add necessary attributes
     if (viewBoxAttr) {
+      var width = parseInt(viewBoxAttr.split(' ')[2], 10);
+      var height = parseInt(viewBoxAttr.split(' ')[3], 10);
+      var widthToHeightRatio = width / height;
+      if (widthToHeightRatio >= 1.5) {
+        $newSvg.addClass('icon--wide');
+      }
       $newSvg.attr('viewBox', viewBoxAttr);
     }
 
     // Add required classes to full color icons
     if (file.relative.indexOf('-full-color') >= 0) {
-      $svg.addClass('icon icon--full-color');
+      $newSvg.addClass('icon--full-color');
     }
 
     $newSvg
-      .addClass('icon')
       .addClass(fileName)
       .append($svg.contents());
 
