a8d0faa0a58d20cd4295a859c2630e1b092cf6a9
Michael Patten <michael.patten@shopify.com>
2016-06-14 14:26:43 -0400

    fix scoping of root check * still requires verification at some point in the near future

--- packages/slate-cli/lib/commands/theme/start.js
+++ packages/slate-cli/lib/commands/theme/start.js
@@ -1,7 +1,8 @@
 var findRoot = require('find-root');
 var utils = require('../../includes/utils.js');
-var themeRoot = findRoot(process.cwd());
+
 
 module.exports = function() {
+  var themeRoot = findRoot(process.cwd());
   utils.runScript(themeRoot, ['start']);
 };
--- packages/slate-cli/lib/commands/theme/watch.js
+++ packages/slate-cli/lib/commands/theme/watch.js
@@ -1,8 +1,8 @@
 var findRoot = require('find-root');
 var utils = require('../../includes/utils.js');
-var themeRoot = findRoot(process.cwd());
 
 module.exports = function(args, options) {
+  var themeRoot = findRoot(process.cwd());
   var scriptArgs = options.nosync
     ? ['watch-nosync']
     : ['watch'];
