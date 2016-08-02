44955bd471122e2d905055424a487ae5e2bac8d5
Michael Patten <michael.patten@shopify.com>
2016-08-01 22:45:39 -0400

    start

--- tasks/js-build.js
+++ tasks/js-build.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
-var include = require('gulp-include');
-var extReplace = require('gulp-ext-replace');
+var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var chokidar = require('chokidar');
+var gutil = require('gulp-util');
+var size = require('gulp-size');
+var stream = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
 
 var config = require('./includes/config.js');
 var messages = require('./includes/messages.js');
@@ -15,37 +21,71 @@ var utils = require('./includes/utilities.js');
  * @memberof slate-cli.tasks.build
  * @static
  */
-gulp.task('build:js', ['lint:js'], function() {
-  processJs();
+gulp.task('build:vendor-js', ['lint:js'], function() {
+  processVendorJs();
 });
 
 /**
- * Watches js in src dir ...
+ * watches js in src dir ...
  *
  * @function watch:js
  * @memberof slate-cli.tasks.watch
  * @static
  */
+gulp.task('watch:vendor-js', function() {
+  chokidar.watch(config.src.vendorJs, {ignoreInitial: true})
+  .on('all', function(event, path) {
+    // messages.logFileEvent(event, path);
+    processVendorJs();
+  });
+});
+
+function processVendorJs() {
+  messages.logProcessFiles('build:vendor-js');
+  return gulp.src(config.src.vendorJs)
+  .pipe(plumber(utils.errorHandler))
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest(config.dist.assets));
+}
+
+
+gulp.task('build:js', function() {
+  var bundler = browserify(config.roots.js, {
+    extensions: ['.js', '.js.liquid'],
+    debug: false
+  });
+
+  return bundle(bundler);
+});
+
 gulp.task('watch:js', function() {
-  chokidar.watch(config.src.js, {ignoreInitial: true})
-    .on('all', function(event, path) {
-      messages.logFileEvent(event, path);
-      processJs();
-    });
+
+  var bundler = browserify({
+    entries: [config.roots.js],
+    extensions: ['.js', '.js.liquid'],
+    debug: false,
+    plugin: [watchify],
+    cache: {},
+    packageCache: {}
+  }).on('update', function() {
+    bundle(bundler);
+  });
+
+  return bundle(bundler);
 });
 
 /**
- * Concatenates JS and copys to the `/dist` folder
  *
- * @param {Array} files
- * @returns {Stream}
+ * @param bundler {Object} - watchify instance
+ * @returns {} -
  * @private
  */
-function processJs() {
-  messages.logProcessFiles('build:js');
-  return gulp.src(config.roots.js)
-    .pipe(plumber(utils.errorHandler))
-    .pipe(include())
-    .pipe(extReplace('.js.liquid'))
-    .pipe(gulp.dest(config.dist.assets));
+function bundle(bundler) {
+  messages.logBundleJs();
+  return bundler.bundle()
+  .on('error', utils.errorHandler)
+  .pipe(stream('theme.js.liquid'))
+  .pipe(buffer())
+  .pipe(size({showFiles: true, pretty: true}))
+  .pipe(gulp.dest(config.dist.assets));
 }
