f0e69cfc24ce7ffb3750fad5b9bcc0513d3be0bf
Steve Bosworth <stevebosworth@users.noreply.github.com>
2016-08-03 15:57:07 -0400

    fixes issues with JS linting during watch (#393)
    * fixes issues with JS linting during watch
    
    * fixes weird streams issue

--- tasks/js-lint.js
+++ tasks/js-lint.js
@@ -12,15 +12,17 @@ gulp.task('lint:js', function() {
 });
 
 gulp.task('watch:js-lint', function() {
-  chokidar.watch([config.src.js, config.tasks], {ignoreInitial: true})
+  chokidar.watch([config.src.js, config.src.jsSections, config.tasks], {ignoreInitial: true})
     .on('all', function() {
       return processLint();
     });
 });
 
 function processLint() {
-  return gulp.src([config.src.js, config.tasks])
+  return gulp.src([config.src.js, config.src.jsSections, config.tasks])
     .pipe(plumber(utils.errorHandler))
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.format())
+    // fixes weird issue with linting during watch
+    .on('data', function() {}); // eslint-disable-line no-empty-function
 }
