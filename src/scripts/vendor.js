e775a710399299304b83e5a3898b85f4a2b127a9
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-09-14 13:42:48 -0400

    add comment about preserve comment

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -4,3 +4,6 @@
 // =require vendor/modernizr.min.js
 /*! jquery-2.2.3.min.js */
 // =require vendor/jquery-2.2.3.min.js
+
+// To preserve a comment in this file, use /*! comment here */
+// this comment will be removed on build.
