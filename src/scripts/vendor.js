0cc4ec71c46f7e5ec58a692eb682c014429de04a
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-09-14 13:44:06 -0400

    update preserve comment

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -5,5 +5,13 @@
 /*! jquery-2.2.3.min.js */
 // =require vendor/jquery-2.2.3.min.js
 
-// To preserve a comment in this file, use /*! comment here */
-// this comment will be removed on build.
+// Attempts to preserve comments that likely contain licensing information,
+// even if the comment does not have directives such as `@license` or `/*!`.
+//
+// Implemented via the [`uglify-save-license`](https://github.com/shinnn/uglify-save-license)
+// module, this option preserves a comment if one of the following is true:
+//
+// 1. The comment is in the *first* line of a file
+// 2. A regular expression matches the string of the comment.
+// 		For example: `MIT`, `@license`, or `Copyright`.
+// 3. There is a comment at the *previous* line, and it matches 1, 2, or 3.
