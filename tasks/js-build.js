ac49379fc7d7970d56f7699717b2b305de1b28b6
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-09-21 15:01:40 -0400

    Update vendor js build (#18)
    * update vendor build and minify
    
    * watch vendor js root
    
    * switch uglify versions
    
    * add file comments
    
    * add comment about preserve comment
    
    * update preserve comment
    
    * fix tab character
    
    * update comment structure
    
    * update to include other root level scripts

--- tasks/js-build.js
+++ tasks/js-build.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var include = require('gulp-include');
 var plumber = require('gulp-plumber');
 var chokidar = require('chokidar');
@@ -10,26 +10,12 @@ var utils = require('./includes/utilities.js');
 
 var lintTask = config.enableLinting ? ['lint:js'] : [];
 
-/**
- * Concatenate JS together into a single file for use in the theme
- *
- * @function build:js
- * @memberof slate-cli.tasks.build
- * @static
- */
 gulp.task('build:vendor-js', function() {
   processVendorJs();
 });
 
-/**
- * watches js in src dir ...
- *
- * @function watch:js
- * @memberof slate-cli.tasks.watch
- * @static
- */
 gulp.task('watch:vendor-js', function() {
-  chokidar.watch(config.src.vendorJs, {ignoreInitial: true})
+  chokidar.watch(config.roots.vendorJs, {ignoreInitial: true})
   .on('all', function(event, path) {
     messages.logFileEvent(event, path);
     processVendorJs();
@@ -38,19 +24,23 @@ gulp.task('watch:vendor-js', function() {
 
 function processVendorJs() {
   messages.logProcessFiles('build:vendor-js');
-  return gulp.src(config.src.vendorJs)
+  return gulp.src(config.roots.vendorJs)
     .pipe(plumber(utils.errorHandler))
-    .pipe(concat('vendor.js'))
+    .pipe(include())
+    .pipe(uglify({
+      mangle: true,
+      compress: true,
+      preserveComments: 'license'
+    }))
     .pipe(gulp.dest(config.dist.assets));
 }
-
 
 gulp.task('build:js', lintTask, function() {
   processThemeJs();
 });
 
 gulp.task('watch:js', function() {
-  chokidar.watch(config.src.js, {ignoreInitial: true})
+  chokidar.watch([config.src.js, '!' + config.roots.vendorJs], {ignoreInitial: true})
   .on('all', function(event, path) {
     messages.logFileEvent(event, path);
     processThemeJs();
@@ -59,7 +49,7 @@ gulp.task('watch:js', function() {
 
 function processThemeJs() {
   messages.logProcessFiles('build:js');
-  return gulp.src(config.roots.js)
+  return gulp.src([config.roots.js, '!' + config.roots.vendorJs])
     .pipe(plumber(utils.errorHandler))
     .pipe(include())
     .pipe(gulp.dest(config.dist.assets));
