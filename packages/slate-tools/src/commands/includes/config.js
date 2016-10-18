7e7815fdc0ae3ebe9e72d91f882198fd469d67e7
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-10-18 08:55:45 -0400

    fix gulp path

--- packages/slate-tools/src/commands/includes/config.js
+++ packages/slate-tools/src/commands/includes/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const findRoot = require('find-root');
 
 const config = {
-  gulpFile: path.join(__dirname, 'gulpfile.js'),
+  gulpFile: path.join(__dirname, '..', '..', 'gulpfile.js'),
   themeRoot: findRoot(process.cwd())
 };
 
