9c5bd70b1555a844c378e435efeb1bea8c7e54ac
Steve Bosworth <stevebosworth@users.noreply.github.com>
2016-11-24 10:58:19 -0500

    Update eslint comments (#30)
    * remove inline comments to fix babel compiling errors
    
    * fixes errors on node v4.x
    
    * got rid of linting error
    
    * updates for clarity

--- packages/slate-tools/src/config.js
+++ packages/slate-tools/src/config.js
@@ -1,14 +1,18 @@
 import {join, normalize} from 'path';
+import {existsSync} from 'fs';
 import findRoot from 'find-root';
 
 const workingDirectory = process.cwd();
 const currentDirectory = __dirname;
 
 const themeRoot = findRoot(workingDirectory);
+const defaultGulpPath = join(themeRoot, normalize('node_modules/.bin/gulp'));
+// Legacy path for older versions of Node.
+const legacyGulpPath = join(themeRoot, normalize('node_modules/@shopify/slate-tools/node_modules/.bin/gulp'));
 
 const config = {
   gulpFile: join(currentDirectory, 'gulpfile.js'),
-  gulp: join(themeRoot, normalize('node_modules/.bin/gulp')),
+  gulp: existsSync(defaultGulpPath) ? defaultGulpPath : legacyGulpPath,
   themeRoot,
 };
 
