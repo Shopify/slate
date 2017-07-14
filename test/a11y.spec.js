52069db9e93ded0ae1fbb7ef2bd972111b126552
Hugues Tennier <hugues.tennier@shopify.com>
2017-07-14 12:02:15 -0400

    Clean a little bit

--- test/a11y.spec.js
+++ test/a11y.spec.js
@@ -1,16 +1,14 @@
-// Add jQuery
 window.$ = require('../src/scripts/vendor/jquery-2.2.3.min');
-// Load a basic template
-const template = require('fs').readFileSync('./test/a11y.spec.html').toString();
-
-require('../src/scripts/slate/utils');
 require('../src/scripts/slate/a11y');
 
+var template = require('fs').readFileSync('./test/a11y.spec.html').toString();
 document.body.innerHTML = template;
+
+var trapFocus = slate.a11y.trapFocus;
 
 describe('a11y.trapFocus', () => {
 
-  slate.a11y.trapFocus({
+  trapFocus({
     $container: $('#modal'),
     $elementToFocus: $('#button'),
     namespace: 'slatenamespace'
@@ -23,5 +21,13 @@ describe('a11y.trapFocus', () => {
   test('Focus on $button when trapFocus is called', () => {
     expect($('#button').is(":focus")).toBe(true);
   });
+
+  // Test namespace
+
+  // Test when there's no $elementToFocus
+
+  // Find a way to test if overall trapFocus works? Maybe loop with tab events and make sure an
+  // element outside #modal is never focused? o_O
+
 
 });
