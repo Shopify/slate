e66146cf5df8931779433e571a224e474627ce1c
t-kelly <thomas.kelly@shopify.com>
2017-10-01 23:18:34 -0400

    Move files to dist folder w/ webpack

--- packages/slate-tools/src/webpack.config.js
+++ packages/slate-tools/src/webpack.config.js
@@ -3,11 +3,24 @@ import config from './config.js';
 import webpack from 'webpack';
 
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 module.exports = {
   entry: path.resolve(config.themeRoot, "src/scripts/theme.js"),
+
   plugins: [
     new CleanWebpackPlugin('dist', {root: config.themeRoot,}),
+
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: '../assets' },
+      { from: 'src/config', to: '../config' },
+      { from: 'src/layout', to: '../layout' },
+      { from: 'src/locales', to: '../locales' },
+      { from: 'src/sections', to: '../sections' },
+      { from: 'src/snippets', to: '../snippets' },
+      { from: 'src/templates', to: '../templates' }
+    ]),
+
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
       minChunks: function (module) {
@@ -19,15 +32,18 @@ module.exports = {
         return module.context && module.context.indexOf("node_modules") !== -1;
       }
     }),
+
     new webpack.optimize.CommonsChunkPlugin({
       name: "manifest",
       minChunks: Infinity
     })
   ],
+
   output: {
     filename: '[name].js',
     path: path.resolve(config.themeRoot, "dist/assets")
   },
+
   module: {
     rules: [
       {
@@ -42,6 +58,7 @@ module.exports = {
       }
     ]
   },
+
   resolveLoader: {
     modules: [
       "./node_modules/@shopify/slate-tools/node_modules"
