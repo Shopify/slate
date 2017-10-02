1fa5472502f0912ef0a16cc4df219640166158cd
t-kelly <thomas.kelly@shopify.com>
2017-10-01 21:33:55 -0400

    Seperate vendor.js, manifest.js and theme.js bundles

--- packages/slate-tools/src/webpack.config.js
+++ packages/slate-tools/src/webpack.config.js
@@ -1,17 +1,31 @@
 import path from "path";
 import config from './config.js';
+import webpack from 'webpack';
 
 import CleanWebpackPlugin from 'clean-webpack-plugin';
-
-console.log(config.themeRoot);
 
 module.exports = {
   entry: path.resolve(config.themeRoot, "src/scripts/theme.js"),
   plugins: [
     new CleanWebpackPlugin('dist', {root: config.themeRoot,}),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      minChunks: function (module) {
+        // This prevents stylesheet resources with the .css or .scss extension
+        // from being moved from their original chunk to the vendor chunk
+        if(module.resource && (/^.*\.(css|scss)$/).test(module.resource)) {
+          return false;
+        }
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "manifest",
+      minChunks: Infinity
+    })
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(config.themeRoot, "dist/assets")
   },
   module: {
@@ -34,5 +48,3 @@ module.exports = {
     ],
   }
 }
-
-console.log(path.resolve(config.themeRoot, "node_modules/@shopify/slate-tools/node_modules"));
