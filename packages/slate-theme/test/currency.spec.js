26b4416e56c33ee82dc0300677133291b42e764a
imontalvomiguel <ubaldo.im@gmail.com>
2017-10-27 03:04:30 -0500

    update test amount_no_decimals_with_comma_separator

--- packages/slate-theme/test/currency.spec.js
+++ packages/slate-theme/test/currency.spec.js
@@ -29,7 +29,7 @@ describe('currency.formatMoney', () => {
     expect(value).toBe('$100');
   });
 
-  test('Formats a number 1000001 to a string of "$10,000"', () => {
+  test('Formats a number 1000001 to a string of "$10.000"', () => {
     var value = formatMoney(1000001, '${{amount_no_decimals_with_comma_separator}}');
     expect(value).toBe('$10.000');
   });
