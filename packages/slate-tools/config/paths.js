2e20fbe3456ac64370080cc9a9f785bbb6924706
t-kelly <thomas.kelly@shopify.com>
2017-11-07 13:24:08 -0500

    Add stylelint to repo and slate-tools

--- packages/slate-tools/config/paths.js
+++ packages/slate-tools/config/paths.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const appDirectory = fs.realpathSync(process.cwd());
 
@@ -18,21 +18,22 @@ function resolveApp(relativePath) {
  * @return String
  */
 function resolveSelf(relativePath) {
-  return path.resolve(__dirname, "../", relativePath);
+  return path.resolve(__dirname, '../', relativePath);
 }
 
 module.exports = {
   root: appDirectory,
-  dist: resolveApp("dist"),
-  src: resolveApp("src"),
-  vendors: resolveApp("src/assets/vendors"),
-  lib: resolveSelf("lib"),
-  nodeModules: resolveSelf("node_modules"),
+  dist: resolveApp('dist'),
+  src: resolveApp('src'),
+  vendors: resolveApp('src/assets/vendors'),
+  lib: resolveSelf('lib'),
+  nodeModules: resolveSelf('node_modules'),
   entrypoints: {
-    scripts: resolveApp("src/assets/scripts/theme.js"),
-    static: resolveSelf("lib/static-files-glob.js")
+    scripts: resolveApp('src/assets/scripts/theme.js'),
+    static: resolveSelf('lib/static-files-glob.js'),
   },
-  assetsOutput: resolveApp("dist/assets"),
-  userShopifyConfig: resolveApp("config/shopify.yml"),
-  eslintrc: resolveApp("./.eslintrc")
+  assetsOutput: resolveApp('dist/assets'),
+  userShopifyConfig: resolveApp('config/shopify.yml'),
+  eslintrc: resolveApp('./.eslintrc'),
+  stylelintrc: resolveApp('./.stylelintrc'),
 };
--- packages/slate-tools/config/webpack.dev.conf.js
+++ packages/slate-tools/config/webpack.dev.conf.js
@@ -1,27 +1,50 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const postcssImport = require('postcss-import');
+const stylelint = require('stylelint');
 
-const config = require("./index");
-const webpackConfig = require("./webpack.base.conf");
-const commonExcludes = require("../lib/common-excludes");
-const userWebpackConfig = require("../lib/get-user-webpack-config")("dev");
+const config = require('./index');
+const webpackConfig = require('./webpack.base.conf');
+const commonExcludes = require('../lib/common-excludes');
+const userWebpackConfig = require('../lib/get-user-webpack-config')('dev');
 
 // so that everything is absolute
 webpackConfig.output.publicPath = `${config.domain}:${config.port}/`;
 
 // add hot-reload related code to entry chunks
-Object.keys(webpackConfig.entry).forEach(name => {
+Object.keys(webpackConfig.entry).forEach((name) => {
   webpackConfig.entry[name] = [
-    path.join(__dirname, "../lib/hot-client.js")
+    path.join(__dirname, '../lib/hot-client.js'),
   ].concat(webpackConfig.entry[name]);
 });
+
+function stylelintLoader() {
+  const rules = [];
+
+  if (fs.existsSync(config.paths.stylelintrc)) {
+    rules.push({
+      loader: 'postcss-loader',
+      options: {
+        plugins: [
+          postcssImport(),
+          stylelint({
+            configFile: config.paths.stylelintrc,
+          }),
+        ],
+      },
+    });
+  }
+
+  return rules;
+}
 
 module.exports = merge(
   webpackConfig,
   {
-    devtool: "#eval-source-map",
+    devtool: '#eval-source-map',
 
     module: {
       rules: [
@@ -29,17 +52,18 @@ module.exports = merge(
           test: /\.s[ac]ss$/,
           exclude: commonExcludes(),
           use: [
-            { loader: "style-loader" },
-            { loader: "css-loader" },
-            { loader: "sass-loader" }
-          ]
-        }
-      ]
+            'style-loader',
+            'css-loader',
+            ...stylelintLoader(),
+            'sass-loader',
+          ],
+        },
+      ],
     },
 
     plugins: [
       new webpack.DefinePlugin({
-        "process.env": { NODE_ENV: '"development"' }
+        'process.env': {NODE_ENV: '"development"'},
       }),
 
       new webpack.HotModuleReplacementPlugin(),
@@ -47,12 +71,12 @@ module.exports = merge(
       new webpack.NoEmitOnErrorsPlugin(),
 
       new HtmlWebpackPlugin({
-        excludeChunks: ["static"],
-        filename: "../layout/theme.liquid",
-        template: "./layout/theme.liquid",
-        inject: true
-      })
-    ]
+        excludeChunks: ['static'],
+        filename: '../layout/theme.liquid',
+        template: './layout/theme.liquid',
+        inject: true,
+      }),
+    ],
   },
   userWebpackConfig
 );
