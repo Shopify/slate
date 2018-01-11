e4fac06b791d0401addde033ca0911ecc1e51c68
t-kelly <thomas.kelly@shopify.com>
2018-01-11 15:00:31 -0500

    Only require shopify.yml if build --deploy flag is passed

--- packages/slate-tools/scripts/build.js
+++ packages/slate-tools/scripts/build.js
@@ -9,7 +9,6 @@ const webpack = require('webpack');
 const webpackConfig = require('../config/webpack.prod.conf');
 const config = require('../config');
 const shopify = require('../lib/shopify-deploy');
-const env = require('../lib/get-shopify-env-or-die')(argv.env, config.shopify);
 
 webpack(webpackConfig, (err, stats) => {
   if (err) throw err;
@@ -25,6 +24,11 @@ webpack(webpackConfig, (err, stats) => {
   );
 
   if (argv.deploy) {
+    const env = require('../lib/get-shopify-env-or-die')(
+      argv.env,
+      config.shopify
+    );
+
     shopify
       .overwrite(env)
       .then(() => {
