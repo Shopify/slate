52583d56591dfb65550db7d676003f35283e6cea
Martin Morissette <martin.morissette@shopify.com>
2018-04-30 11:58:51 -0400

    [slate-cssvar-loader] Escape double quotes from liquid variable

--- packages/slate-cssvar-loader/index.js
+++ packages/slate-cssvar-loader/index.js
@@ -24,11 +24,15 @@ function parseCSSVariables(cssVariablesPaths) {
       let cssVariableDecl;
       while ((cssVariableDecl = CSS_VAR_DECL_REGEX.exec(styleBlock)) != null) {
         const [, cssVariable, liquidVariable] = cssVariableDecl;
-        variables[cssVariable] = liquidVariable;
+        variables[cssVariable] = escapeLiquidVariable(liquidVariable);
       }
     }
   });
   return variables;
+}
+
+function escapeLiquidVariable(variable) {
+  return variable.replace(/"/g, '\\"');
 }
 
 function SlateCSSLoader(source) {
