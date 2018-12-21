c3ef5158bb4f028db28b03ebefc043f0c96870b1
Hugues Tennier <hugues.tennier@shopify.com>
2018-12-21 13:14:58 -0500

    Add slate versions link for mobile only

--- docusaurus/v1/siteConfig.js
+++ docusaurus/v1/siteConfig.js
@@ -25,7 +25,10 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [{href: 'https://github.com/Shopify/slate', label: 'GitHub'}],
+  headerLinks: [
+    {href: '/slate/versions', label: 'Slate Versions'},
+    {href: 'https://github.com/Shopify/slate', label: 'GitHub'},
+  ],
 
   /* path to images for header/footer */
   headerIcon: 'img/slate.svg',
