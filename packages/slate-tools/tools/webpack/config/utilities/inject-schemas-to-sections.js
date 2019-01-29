34b07bfece79fad945e605e0f02c3dacce605ed5
Mikhail <durnevM@mail.ru>
2019-01-29 19:10:05 +0300

    Update inject-schemas-to-sections.js

--- packages/slate-tools/tools/webpack/config/utilities/inject-schemas-to-sections.js
+++ packages/slate-tools/tools/webpack/config/utilities/inject-schemas-to-sections.js
@@ -1,49 +1,46 @@
 const fs = require('fs');
 
 module.exports = function(content, path) {
-
-    // naming convention is : section_name.schema.json for separate json schema files.
-    const jsonSchemaFilePath = path.replace('.liquid', '.schema.json');
-
-    // lets check if schema file exists and 
-
-    if(fs.existsSync(jsonSchemaFilePath)){
+  // naming convention is : section_name.schema.json for separate json schema files.
+  const jsonSchemaFilePath = path.replace('.liquid', '.schema.json');
+  let transformedContent = content;
+  // lets check if schema file exists and
+  if (fs.existsSync(jsonSchemaFilePath)) {
     
-        console.log(`Found separate section schema for: ${path}`);
-
-        content = content.toString();
-
-        // and check if we already have defined {% schema %} in content
-        if(!content.match(/{\%\s*schema\s*\%}([\s\S]+){\%\s*endschema\s*\%}/gmi)){
-
-            try{
-            const jsonSchema = fs.readFileSync(jsonSchemaFilePath, 'utf8');
-                            
-            console.log(`Injecting JSON Schema ${jsonSchemaFilePath}  to Section at ${path}`);
+    console.log(`Found separate section schema for: ${path}`);
     
-                if(jsonSchema){
-                    content = 
+    transformedContent = content.toString();
+
+    // and check if we already have defined {% schema %} in content
+    if (!content.match(/{\%\s*schema\s*\%}([\s\S]+){\%\s*endschema\s*\%}/gmi)) {
+      try {
+        const jsonSchema = fs.readFileSync(jsonSchemaFilePath, 'utf8');
+        
+        console.log(`Injecting JSON Schema ${jsonSchemaFilePath}  to Section at ${path}`);
+        
+        if (jsonSchema) {
+          content =
 `${content}
 {% schema %}
   ${jsonSchema}
 {% endschema %}
 `;
-                }else{
-                    console.warn(` No Schema defined for Section at: ${path}`);  
-                }
-            }catch(e){
-                console.warn(e,` Error reading  json schema file: ${jsonSchemaFilePath} for ${path}`);
-            }
-      }else{
-        console.warn(`!----------------------------------------------------------! `);
-        console.warn(` You have json schema file: ${jsonSchemaFilePath} for ${path} `);
-        console.warn(` but it contains {% schema %}...{% endschema %} declaration`);
-        console.warn(` please remove it from ${path} `);
-        console.warn(` to allow me to inject schema from separate .schema.json file `);
-        console.warn(`------------------------------------------------------------ `);
+        }else{
+          console.warn(` No Schema defined for Section at: ${path}`);
+        }
+      }catch(e){
+        console.warn(e,` Error reading  json schema file: ${jsonSchemaFilePath} for ${path}`);
       }
+    }else{
+      console.warn(`!----------------------------------------------------------! `);
+      console.warn(` You have json schema file: ${jsonSchemaFilePath} for ${path} `);
+      console.warn(` but it contains {% schema %}...{% endschema %} declaration`);
+      console.warn(` please remove it from ${path} `);
+      console.warn(` to allow me to inject schema from separate .schema.json file `);
+      console.warn(`------------------------------------------------------------ `);
     }
-    // return content untouched if schema is already defined in it. or we don't have separate json file
-    return content;
+  }
+  // return content untouched if schema is already defined in it. or we don't have separate json file
+  return transformedContent;
 
 }
