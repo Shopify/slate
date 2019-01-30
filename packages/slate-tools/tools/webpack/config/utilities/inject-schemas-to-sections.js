e066c97b33c141f8d5d7ecc9ab467d4b611e9f23
Mikhail <durnevM@mail.ru>
2019-01-30 11:15:37 +0300

    Update inject-schemas-to-sections.js
    added `{% inject 'file_name_to_inject.json|whatever' %}`  path of file to inject is relative to liquid file we inject to, and is resolved via `path.resolve()`.
    NB: approach to insert {% schema %} with a separate file named `section-name.schema.json` will work only if there's no `{% schema %}` tag defined in liquid file, so these approaches will not conflict or overwrite each other.

--- packages/slate-tools/tools/webpack/config/utilities/inject-schemas-to-sections.js
+++ packages/slate-tools/tools/webpack/config/utilities/inject-schemas-to-sections.js
@@ -1,25 +1,33 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports = function(content, path) {
+module.exports = function(content, liquidFilePath) {
+
+  let transformedContent = content.toString();
+
   // naming convention is : section_name.schema.json for separate json schema files.
-  const jsonSchemaFilePath = path.replace('.liquid', '.schema.json');
-  let transformedContent = content;
+  const jsonSchemaFilePath = liquidFilePath.replace('.liquid', '.schema.json');
+
+  // to check if we already have defined {% schema %} in content
+  const schemaTagExistsInFileRegex = /{%\s*schema\s*%}([\s\S]+){%\s*endschema\s*%}/gmi;
+  // check for {% inject 'file.schema.json' %}
+  const injectorRegex = /{%\s+inject\s+['"]([^'"]+)['"]\s*%}/gmi;
+  const schemaTagExists = transformedContent.match(schemaTagExistsInFileRegex);
+
   // lets check if schema file exists and
   if (fs.existsSync(jsonSchemaFilePath)) {
 
-    console.log(`Found separate section schema for: ${path}`);
-    
-    transformedContent = content.toString();
-
-    // and check if we already have defined {% schema %} in content
-    if (!(transformedContent.match(/{%\s*schema\s*%}([\s\S]+){%\s*endschema\s*%}/gim))) {
+    if (schemaTagExists) {
+      // do nothing maybe warn about to inject it with {% inject 'file.json' %}
+    } else {
       try {
         const jsonSchema = fs.readFileSync(jsonSchemaFilePath, 'utf8');
 
         console.log(
-          `Injecting JSON Schema ${jsonSchemaFilePath}  to Section at ${path}`
+          `Found separate section schema for: ${liquidFilePath}`,
+          `Injecting JSON Schema ${jsonSchemaFilePath}  to Section at ${liquidFilePath}`,
         );
-        
+
         if (jsonSchema) {
           transformedContent =
 `${transformedContent}
@@ -27,37 +35,55 @@ module.exports = function(content, path) {
   ${jsonSchema}
 {% endschema %}
 `;
-        }else{
-          console.warn(` No Schema defined for Section at: ${path}`);
+        } else {
+          console.warn(` No Schema defined for Section at: ${liquidFilePath}`);
         }
-      }catch(exceptionObj){
+      } catch (exceptionObj) {
         console.warn(
           exceptionObj,
-          ` Error reading  json schema file: ${jsonSchemaFilePath} for ${path}`
+          ` Error reading  json schema file: ${jsonSchemaFilePath} for ${liquidFilePath}`,
         );
       }
-    }else{
-      console.warn(
-        `!----------------------------------------------------------! `
-      );
-      console.warn(
-        ` You have json schema file: ${jsonSchemaFilePath} for ${path} `
-      );
-      console.warn(
-        ` but it contains {% schema %}...{% endschema %} declaration`
-      );
-      console.warn(
-        ` please remove it from ${path} `)
-      ;
-      console.warn(
-        ` to allow me to inject schema from separate .schema.json file `
-      );
-      console.warn(
-        `------------------------------------------------------------ `
-      );
     }
   }
-  // return content untouched if schema is already defined in it. or we don't have separate json file
+
+  // lets inject all we want here :)
+  if (transformedContent.match(injectorRegex)) {
+    // lets check if injected file exists and inject it if needed
+    let injectorMatch;
+
+    while ((injectorMatch = injectorRegex.exec(transformedContent)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (injectorMatch.index === injectorRegex.lastIndex) {
+        injectorRegex.lastIndex++;
+      }
+      // run over inject commands and inject files
+      const injectorTagToReplace = injectorMatch[0];
+      const injectedFileName = injectorMatch[1];
+
+      if (injectorTagToReplace && injectedFileName) {
+        const fileToInjectPath = path.resolve(path.dirname(liquidFilePath), injectedFileName);
+
+        if (fs.existsSync(fileToInjectPath)) {
+          const injectedFileContent = fs.readFileSync(fileToInjectPath, 'utf8');
+
+          console.log(
+            `Injecting ${fileToInjectPath} to ${liquidFilePath} at ${injectorTagToReplace}`,
+          );
+          transformedContent = transformedContent.replace(injectorTagToReplace, injectedFileContent);
+
+        } else {
+          console.warn(
+            ` Inject command in ${liquidFilePath} can't find file to inject at ${fileToInjectPath}`,
+          );
+        }
+      }
+    }
+
+  }
+
+
+  // return content untouched if schema is already defined in it. or we don't have separate json file, or there's no {% inject 'filename' %}
   return transformedContent;
 
 };
