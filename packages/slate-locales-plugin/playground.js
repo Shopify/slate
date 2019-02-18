652290a55b9746da368ccd6f383641b60cea5492
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-18 16:29:13 -0500

    working plugin, tests need to be redone

---
+++ packages/slate-locales-plugin/playground.js
@@ -0,0 +1,9 @@
+const compiler = require('./__tests__/helpers/compiler');
+
+// eslint-disable-next-line func-style
+const test = async () => {
+  const stats = await compiler('fixtures/normalsections/');
+  debugger;
+};
+
+test();
