6fdf0289a8ad6b6d5b94cc3baa35994f63f9fb44
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-18 20:26:38 -0500

    Basic tests and forgot schema tag

--- packages/slate-locales-plugin/playground.js
+++ packages/slate-locales-plugin/playground.js
@@ -2,7 +2,7 @@ const compiler = require('./__tests__/helpers/compiler');
 
 // eslint-disable-next-line func-style
 const test = async () => {
-  const stats = await compiler('fixtures/normalsections/');
+  const stats = await compiler('fixtures/seperatejsonsections/');
   debugger;
 };
 
