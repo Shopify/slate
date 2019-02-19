c92a13190ef7a711bd90d731dc6c15efc15e434d
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-18 20:47:47 -0500

    Using lodash instead of manual recursion

--- packages/slate-locales-plugin/playground.js
+++ packages/slate-locales-plugin/playground.js
@@ -2,7 +2,7 @@ const compiler = require('./__tests__/helpers/compiler');
 
 // eslint-disable-next-line func-style
 const test = async () => {
-  const stats = await compiler('fixtures/seperatejsonsections/');
+  const stats = await compiler('fixtures/normalsections/');
   debugger;
 };
 
