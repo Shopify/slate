48c7b6fee326bbf0b25b8f4c2571c4f3beb8df6b
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-21 10:45:48 -0500

    More descriptive tests and updated tests

--- packages/slate-sections-plugin/__tests__/compiler-tests.js
+++ packages/slate-sections-plugin/__tests__/compiler-tests.js
@@ -20,7 +20,24 @@ test('sections with no seperate schemas, with liquid files that just need to be 
   });
 });
 
-test('sections that have templates living in folders with a schema.json and potentially locales to go with aswell', async () => {
+test('section that has template living in folders with schema.json and no locales', async () => {
+  const stats = await compiler('fixtures/seperatejsonsections/');
+  expect(stats.compilation.assets).toMatchSnapshot();
+
+  const outputFiles = stats.compilation.compiler.outputFileSystem.readdirSync(
+    path.resolve(__dirname, 'dist', 'sections'),
+  );
+  outputFiles.forEach((file) => {
+    expect(
+      stats.compilation.compiler.outputFileSystem.readFileSync(
+        path.resolve(__dirname, 'dist', 'sections', file),
+        'utf-8',
+      ),
+    ).toMatchSnapshot();
+  });
+});
+
+test('sections that have templates living in folders with a schema.json and locales to go with aswell', async () => {
   const stats = await compiler('fixtures/normalsections/');
   // Check if file has been added to assets so webpack can output it
   expect(
@@ -29,10 +46,14 @@ test('sections that have templates living in folders with a schema.json and pote
   ).toMatchSnapshot();
 
   // Check if file gets written to the output file system at the correct location, and with the correct values
+
   expect(
-    stats.compilation.compiler.outputFileSystem.readFileSync(
-      path.resolve(__dirname, 'dist', 'sections', 'test-section.liquid'),
-      'utf-8',
+    JSON.parse(
+      stats.compilation.assets[
+        '../sections/test-section.liquid'
+      ].children[1]._value
+        .replace('{% schema %}', '')
+        .replace('{% endschema %}', ''),
     ),
   ).toMatchSnapshot();
 });
