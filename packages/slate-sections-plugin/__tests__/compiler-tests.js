c6323d85df5af8ebdda8d96c9d658323cc9a5ebc
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-21 07:52:27 -0500

    Lot of changes to clean up

--- packages/slate-sections-plugin/__tests__/compiler-tests.js
+++ packages/slate-sections-plugin/__tests__/compiler-tests.js
@@ -20,28 +20,12 @@ test('sections with no seperate schemas, with liquid files that just need to be 
   });
 });
 
-test('sections with multiple liquid files with corresponding json files with the same name that require the json to be appended as a schema', async () => {
-  const stats = await compiler('fixtures/seperatejsonsections/');
-  expect(stats.compilation.assets).toMatchSnapshot();
-
-  const outputFiles = stats.compilation.compiler.outputFileSystem.readdirSync(
-    path.resolve(__dirname, 'dist', 'sections'),
-  );
-  outputFiles.forEach((file) => {
-    expect(
-      stats.compilation.compiler.outputFileSystem.readFileSync(
-        path.resolve(__dirname, 'dist', 'sections', file),
-        'utf-8',
-      ),
-    ).toMatchSnapshot();
-  });
-});
-
 test('sections that have templates living in folders with a schema.json and potentially locales to go with aswell', async () => {
   const stats = await compiler('fixtures/normalsections/');
   // Check if file has been added to assets so webpack can output it
   expect(
-    stats.compilation.assets['../sections/test-section.liquid'],
+    stats.compilation.assets['../sections/test-section.liquid'].children[0]
+      ._value,
   ).toMatchSnapshot();
 
   // Check if file gets written to the output file system at the correct location, and with the correct values
--- packages/slate-sections-plugin/index.js
+++ packages/slate-sections-plugin/index.js
@@ -23,125 +23,134 @@ module.exports = class sectionsPlugin {
     files.forEach(async (file) => {
       const fileStat = await fs.stat(path.resolve(this.options.from, file));
       if (fileStat.isDirectory()) {
-        traverseDirectory(compilation, path.resolve(this.options.from, file));
+        this._handleSectionDirectory(
+          path.resolve(this.options.from, file),
+          compilation,
+        );
       } else if (fileStat.isFile() && path.extname(file) === '.liquid') {
-        getSchema(file, this.options.from, compilation, files);
+        this._addLiquidFileToAssets(
+          path.resolve(this.options.from, file),
+          compilation,
+        );
       }
     });
   }
-};
 
-async function getSchema(
-  fileName,
-  directoryPath,
-  compilation,
-  filesInDirectory,
-) {
-  const liquidFile = await fs.readFile(
-    path.resolve(directoryPath, fileName),
-    'utf-8',
-  );
-  let schemaFile = null;
-
-  const outputKey = getOutputKey(fileName, directoryPath, compilation);
-  if (
-    fileName === GENERIC_TEMPLATE_NAME &&
-    filesInDirectory.includes('locales') &&
-    filesInDirectory.includes('schema.json')
-  ) {
-    const combinedLocales = await combineLocales(
-      path.resolve(directoryPath, 'locales'),
-    );
-    schemaFile = JSON.stringify(
-      await createMainSchema(
-        combinedLocales,
-        path.resolve(directoryPath, 'schema.json'),
-      ),
-    );
-  } else if (filesInDirectory.includes(fileName.replace('.liquid', '.json'))) {
-    schemaFile = await fs.readFile(
-      path.resolve(directoryPath, fileName.replace('.liquid', '.json')),
-    );
-  }
-
-  if (schemaFile) {
-    compilation.assets[outputKey] = new ConcatSource(
-      new RawSource(liquidFile),
-      new RawSource(`{% schema %}\n ${schemaFile}{% endschema %}`),
-    );
-  } else {
-    compilation.assets[outputKey] = new RawSource(liquidFile);
-  }
-}
-
-function getOutputKey(fileName, directoryPath, compilation) {
-  const outputFileName =
-    fileName === GENERIC_TEMPLATE_NAME
-      ? `${directoryPath.split('/sections/')[1]}.liquid`
-      : fileName;
-  const outputPath = compilation.compiler.outputPath;
-  const sectionsOutputPath = `${outputPath.split('/dist')[0]}/dist/sections`;
-  const relativePathToSections = path.relative(outputPath, sectionsOutputPath);
-
-  return `${relativePathToSections}/${outputFileName}`;
-}
-
-async function traverseDirectory(compilation, directoryPath) {
-  const files = await fs.readdir(directoryPath);
-  files.forEach(async (file) => {
-    const fileStat = await fs.stat(path.resolve(directoryPath, file));
-    if (fileStat.isDirectory() && file !== 'locales') {
-      traverseDirectory(compilation, path.resolve(directoryPath, file));
-    } else if (fileStat.isFile() && path.extname(file) === '.liquid') {
-      getSchema(file, directoryPath, compilation, files);
+  _getOutputFileName(relativePathFromSections) {
+    if (relativePathFromSections.includes(GENERIC_TEMPLATE_NAME)) {
+      return relativePathFromSections.replace(
+        `/${GENERIC_TEMPLATE_NAME}`,
+        '.liquid',
+      );
+    } else {
+      return relativePathFromSections;
     }
-  });
-}
-
-function getLocalizedValues(key, mainSchema, localizedSchema) {
-  // eslint-disable-next-line
-  let combinedTranslationsObject = {};
-  // eslint-disable-next-line
-  for (const i in localizedSchema) {
-    combinedTranslationsObject[i] = _.get(localizedSchema[i], key);
   }
 
-  return combinedTranslationsObject;
-}
+  _getOutputKey(liquidSourcePath, compilation) {
+    const relativePathFromSections = liquidSourcePath.replace(
+      `${this.options.from}/`,
+      '',
+    );
 
-async function createMainSchema(localizedSchema, mainSchemaPath) {
-  function traverse(obj) {
-    for (const i in obj) {
-      if (typeof obj[i].t === 'string') {
-        obj[i] = getLocalizedValues(obj[i].t, obj, localizedSchema);
-      } else if (typeof obj[i] === 'object') {
-        traverse(obj[i]);
+    const fileName = this._getOutputFileName(relativePathFromSections);
+
+    // The relative path form the output set in webpack, to the specified output for sections in slate config
+    const relativeOutputPath = path.relative(
+      compilation.compiler.outputPath,
+      this.options.to,
+    );
+
+    return `${relativeOutputPath}/${fileName}`;
+  }
+
+  async _addLiquidFileToAssets(sourcePath, compilation, schemaSource = null) {
+    const liquidContent = await fs.readFile(sourcePath, 'utf-8');
+    const liquidSource = new RawSource(liquidContent);
+    const outputKey = this._getOutputKey(sourcePath, compilation);
+
+    if (schemaSource) {
+      compilation.assets[outputKey] = new ConcatSource(
+        liquidSource,
+        schemaSource,
+      );
+    } else {
+      compilation.assets[outputKey] = liquidSource;
+    }
+  }
+
+  async _handleSectionDirectory(directoryPath, compilation) {
+    const files = await fs.readdir(directoryPath);
+
+    const liquidSourcePath = path.resolve(directoryPath, GENERIC_TEMPLATE_NAME);
+    if (files.includes('schema.json')) {
+      const combinedLocales = files.includes('locales')
+        ? await this._combineLocales(path.resolve(directoryPath, 'locales'))
+        : null;
+
+      const schemaContent = combinedLocales
+        ? await this._createSchemaContentWithLocales(
+            combinedLocales,
+            path.resolve(directoryPath, 'schema.json'),
+          )
+        : await fs.readFile(path.resolve(directoryPath, 'schema.json'));
+      const schemaSource = new RawSource(
+        `{% schema %}\n${schemaContent}{% endschema %}`,
+      );
+      this._addLiquidFileToAssets(liquidSourcePath, compilation, schemaSource);
+    } else {
+      this._addLiquidFileToAssets(liquidSourcePath, compilation);
+    }
+  }
+
+  _getLocalizedValues(key, mainSchema, localizedSchema) {
+    // eslint-disable-next-line
+    let combinedTranslationsObject = {};
+    // eslint-disable-next-line
+    for (const i in localizedSchema) {
+      combinedTranslationsObject[i] = _.get(localizedSchema[i], key);
+    }
+
+    return combinedTranslationsObject;
+  }
+
+  async _createSchemaContentWithLocales(localizedSchema, mainSchemaPath) {
+    const traverse = (obj) => {
+      for (const i in obj) {
+        if (typeof obj[i].t === 'string') {
+          obj[i] = this._getLocalizedValues(obj[i].t, obj, localizedSchema);
+        } else if (typeof obj[i] === 'object') {
+          traverse(obj[i]);
+        }
       }
-    }
-    return obj;
+      return JSON.stringify(obj);
+    };
+    // eslint-disable-next-line
+    let mainSchema = JSON.parse(await fs.readFile(mainSchemaPath, 'utf-8'));
+    return traverse(mainSchema);
   }
-  // eslint-disable-next-line
-  let mainSchema = JSON.parse(await fs.readFile(mainSchemaPath, 'utf-8'));
-  return traverse(mainSchema);
-}
 
-async function combineLocales(localesPath) {
-  const localesFiles = await fs.readdir(localesPath);
-  const jsonFiles = localesFiles.filter((fileName) =>
-    fileName.endsWith('.json'),
-  );
-
-  // eslint-disable-next-line prefer-const
-  let accumulator = {};
-  await jsonFiles.map(async (file) => {
-    const localeCode = path
-      .basename(file)
-      .split('.')
-      .shift();
-    const fileContents = JSON.parse(
-      await fs.readFile(path.resolve(localesPath, file), 'utf-8'),
+  async _combineLocales(localesPath) {
+    const localesFiles = await fs.readdir(localesPath);
+    const jsonFiles = localesFiles.filter((fileName) =>
+      fileName.endsWith('.json'),
     );
-    accumulator[localeCode] = fileContents;
-  });
-  return accumulator;
-}
+
+    // eslint-disable-next-line prefer-const
+    let accumulator = {};
+    await Promise.all(
+      jsonFiles.map(async (file) => {
+        const localeCode = path
+          .basename(file)
+          .split('.')
+          .shift();
+        const fileContents = JSON.parse(
+          await fs.readFile(path.resolve(localesPath, file), 'utf-8'),
+        );
+        accumulator[localeCode] = fileContents;
+      }),
+    );
+
+    return accumulator;
+  }
+};
