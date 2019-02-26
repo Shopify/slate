ecc2cf52a735daeae84775bd761793a0152db8b1
Harshal Brahmbhatt <harshal.brahmbhatt@shopify.com>
2019-02-26 15:33:07 -0500

    Assigning output key to variable in test suite

--- packages/slate-sections-plugin/__tests__/index.test.js
+++ packages/slate-sections-plugin/__tests__/index.test.js
@@ -5,8 +5,9 @@ jest.unmock('fs-extra');
 
 test('sections with no seperate schemas, with liquid files that just need to be copied over', async () => {
   const stats = await compiler('fixtures/startersections/');
+  const expectedAssetOutputKey = '../sections/test-section.liquid';
   expect(
-    stats.compilation.assets['../sections/test-section.liquid'].__value,
+    stats.compilation.assets[expectedAssetOutputKey].__value,
   ).toMatchSnapshot();
 
   const outputFiles = stats.compilation.compiler.outputFileSystem.readdirSync(
@@ -24,13 +25,12 @@ test('sections with no seperate schemas, with liquid files that just need to be 
 
 test('section that has template living in folders with schema.json and no locales', async () => {
   const stats = await compiler('fixtures/seperatejsonsections/');
+  const expectedAssetOutputKey = '../sections/test-section.liquid';
   expect(
-    stats.compilation.assets['../sections/test-section.liquid'].children[0]
-      .__value,
+    stats.compilation.assets[expectedAssetOutputKey].children[0].__value,
   ).toMatchSnapshot();
   expect(
-    stats.compilation.assets['../sections/test-section.liquid'].children[1]
-      .__value,
+    stats.compilation.assets[expectedAssetOutputKey].children[1].__value,
   ).toMatchSnapshot();
 
   const outputFiles = stats.compilation.compiler.outputFileSystem.readdirSync(
@@ -49,18 +49,18 @@ test('section that has template living in folders with schema.json and no locale
 test('sections that have templates living in folders with a schema.json and locales to go with aswell', async () => {
   const stats = await compiler('fixtures/normalsections/');
   // Check if file has been added to assets so webpack can output it
+
+  const expectedAssetOutputKey = '../sections/test-section.liquid';
   expect(
-    stats.compilation.assets['../sections/test-section.liquid'].children[0]
-      ._value,
+    stats.compilation.assets[expectedAssetOutputKey].children[0]._value,
   ).toMatchSnapshot();
 
   // Check if file gets written to the output file system at the correct location, and with the correct values
 
+  // Remove the schema tags and compare the json object to snapshot
   expect(
     JSON.parse(
-      stats.compilation.assets[
-        '../sections/test-section.liquid'
-      ].children[1]._value
+      stats.compilation.assets[expectedAssetOutputKey].children[1]._value
         .replace('{% schema %}', '')
         .replace('{% endschema %}', ''),
     ),
