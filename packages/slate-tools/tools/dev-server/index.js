bb43e56c9aa47be1e1f596d70f497eb098c1b988
Steve Kanter <197086+stevekanter-arcticleaf@users.noreply.github.com>
2019-07-18 11:59:26 -0400

    Disable HSTS for dev server
    Most other non-Slate 1.x development is via HTTP so by having HSTS on it really makes Shopify development tricky.

--- packages/slate-tools/tools/dev-server/index.js
+++ packages/slate-tools/tools/dev-server/index.js
@@ -29,6 +29,12 @@ class DevServer {
           req.url += prefix + queryStringComponents.join('&');
           next();
         },
+        proxyRes: [
+            function(proxyRes, req, res) {
+                // disable HSTS. Slate might force us to use HTTPS but having HSTS on local dev makes it impossible to do other non-Slate dev.
+                delete proxyRes.headers['strict-transport-security'];
+            }
+        ]
       },
       https: {key: getSSLKeyPath(), cert: getSSLCertPath()},
       logLevel: 'silent',
