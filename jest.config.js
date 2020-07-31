ce7b60862f9674a35051d08b7ac72b33577deefa
Dom Masters <dom@processcreative.com.au>
2020-07-31 13:51:24 +1000

    Removed more un-required packages

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/lib/',
-    'packages/slate-tools/src/commands/test.js',
-    '__tests__/fixtures/',
-    '__tests__/helpers',
-  ],
-};
