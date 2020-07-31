f4254e1c666000a8964a008041c9b8661af17f03
Dom Masters <dom@processcreative.com.au>
2020-07-31 15:33:28 +1000

    Removed slate rc and slate error

--- packages/slate-rc/slate-rc-error.js
+++
@@ -1,6 +0,0 @@
-module.exports = class SlateRcError extends require('@process-creative/slate-error') {
-  constructor(message) {
-    // Providing default message and overriding status code.
-    super(message || 'There was an error when processing the .slaterc file');
-  }
-};
--- packages/slate-rc/slate-rc.schema.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const os = require('os');
-
-module.exports = {
-  // Directory to look for .slaterc file
-  'paths.slateRc': path.resolve(os.homedir(), '.slaterc'),
-};
