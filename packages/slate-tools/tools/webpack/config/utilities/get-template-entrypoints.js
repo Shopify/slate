da8a303343abac45fd9c614a33f5a8834a9079f4
Albert Sta Cruz <albert.stacruz@gmail.com>
2020-09-18 12:58:02 -0700

    Add product.js to all product template webpack entry points

--- packages/slate-tools/tools/webpack/config/utilities/get-template-entrypoints.js
+++ packages/slate-tools/tools/webpack/config/utilities/get-template-entrypoints.js
@@ -4,65 +4,71 @@ const SlateConfig = require('@shopify/slate-config');
 const config = new SlateConfig(require('../../../../slate-tools.schema'));
 
 const VALID_LIQUID_TEMPLATES = [
-  '404',
-  'article',
-  'blog',
-  'cart',
-  'collection',
-  'account',
-  'activate_account',
-  'addresses',
-  'login',
-  'order',
-  'register',
-  'reset_password',
-  'gift_card',
-  'index',
-  'list-collections',
-  'page',
-  'password',
-  'product',
-  'search',
+  '404',
+  'article',
+  'blog',
+  'cart',
+  'collection',
+  'account',
+  'activate_account',
+  'addresses',
+  'login',
+  'order',
+  'register',
+  'reset_password',
+  'gift_card',
+  'index',
+  'list-collections',
+  'page',
+  'password',
+  'product',
+  'search',
 ];
 
 function isValidTemplate(filename) {
-  const name = VALID_LIQUID_TEMPLATES.filter((template) =>
-    filename.startsWith(`${template}.`),
-  );
-  return Boolean(name);
+  const name = VALID_LIQUID_TEMPLATES.filter((template) =>
+    filename.startsWith(`${template}.`),
+  );
+  return Boolean(name);
 }
 
 module.exports = function() {
-  const entrypoints = {};
+  const entrypoints = {};
 
-  fs.readdirSync(config.get('paths.theme.src.templates')).forEach((file) => {
-    const name = path.parse(file).name;
-    const jsFile = path.join(
-      config.get('paths.theme.src.scripts'),
-      'templates',
-      `${name}.js`,
-    );
+  fs.readdirSync(config.get('paths.theme.src.templates')).forEach((file) => {
+    const name = path.parse(file).name;
+    const jsFile = path.join(
+      config.get('paths.theme.src.scripts'),
+      'templates',
+      `${name}.js`,
+    );
 
-    if (isValidTemplate(name) && fs.existsSync(jsFile)) {
-      entrypoints[`template.${name}`] = jsFile;
-    }
-  });
+    if ((isValidTemplate(name) && fs.existsSync(jsFile) && !name.includes('product')) || (isValidTemplate(name) && fs.existsSync(jsFile) && name == 'product')) {
+      entrypoints[`template.${name}`] = jsFile;
+    } else if (isValidTemplate(name) && fs.existsSync(jsFile) && name.includes('product') && name !== 'product') {
+      entrypoints[`template.${name}`] = [jsFile, path.join(
+        config.get('paths.theme.src.scripts'),
+        'templates',
+        `product.js`,
+      )];
+    } 
+  });
 
-  fs
-    .readdirSync(config.get('paths.theme.src.templates.customers'))
-    .forEach((file) => {
-      const name = `${path.parse(file).name}`;
-      const jsFile = path.join(
-        config.get('paths.theme.src.scripts'),
-        'templates',
-        'customers',
-        `${name}.js`,
-      );
+  fs
+    .readdirSync(config.get('paths.theme.src.templates.customers'))
+    .forEach((file) => {
+      const name = `${path.parse(file).name}`;
+      const jsFile = path.join(
+        config.get('paths.theme.src.scripts'),
+        'templates',
+        'customers',
+        `${name}.js`,
+      );
 
-      if (isValidTemplate(name) && fs.existsSync(jsFile)) {
-        entrypoints[`template.${name}`] = jsFile;
-      }
-    });
+      if (isValidTemplate(name) && fs.existsSync(jsFile)) {
+        entrypoints[`template.${name}`] = jsFile;
+      }
+    });
 
-  return entrypoints;
+  return entrypoints;
 };
